9280048d0113116fa76a77bb1d139978e17f1ab7
Ali925 <alitural.mehdi@gmail.com>
2017-09-18 01:36:37 +0400

    layers

--- app/scripts/controllers/sectors.js
+++ app/scripts/controllers/sectors.js
@@ -30,25 +30,33 @@ angular.module('yapp')
 				attributionControl: true
 		};
   	$scope.map.tiles = {};
-		$scope.map.layers = {};
   	$scope.map.height = $(window).height()*0.82 + 'px';
 
   	var auth_token = 'pk.eyJ1IjoiYWxpOTI1IiwiYSI6ImNpcHZobHIxMzAwNTZpMWtzcmFrdmk2OXcifQ.7AB_LOCsVI6kUn_vPDUXkg';
 
   	$scope.map.tiles.url = 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + auth_token;
 	
-		$scope.map.layers.overlays = {};
-	
-		$scope.map.layers.baselayers = {
-			mapboxGlLayer: {
-        name: 'Sample',
-        type: 'mapboxGL',
-        layerOptions: {
-          accessToken: 'pk.eyJ1IjoiZ3JlZW5jaDRhbGV4IiwiYSI6ImNpa242d2dtMzAwOGh3YW02dDd6eGIwb28ifQ.V8B9D1mUvD21JCkPRxpKxA',
-          style: 'mapbox://styles/greench4alex/cita9atqv000e2hr7v4htiycv'
-        }
-			}
-		};
+		angular.extend($scope, {
+              center: {
+                lat: 25,
+								lng: 50,
+								zoom: 2
+              },
+              layers: {
+                baselayers: {
+                  mapboxGlLayer: {
+										name: 'Sample',
+										type: 'mapboxGL',
+										layerOptions: {
+											accessToken: 'pk.eyJ1IjoiZ3JlZW5jaDRhbGV4IiwiYSI6ImNpa242d2dtMzAwOGh3YW02dDd6eGIwb28ifQ.V8B9D1mUvD21JCkPRxpKxA',
+											style: 'mapbox://styles/greench4alex/cita9atqv000e2hr7v4htiycv'
+										}
+									}
+                },
+                overlays: {}
+              }
+            });
+
 
   	$scope.map.initMap = function(){
 
